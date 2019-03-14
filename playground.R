4b40c56b9e094d8500a7598cce3ce13a32a7b60c
indiflex <indiflex1@gmail.com>
2019-03-14 10:11:24 +0900

    r md

--- playground.R
+++ playground.R
@@ -802,3 +802,186 @@ class(mpg$fl)
 
 mpg = inner_join(mpg, flpr, by=c('fl', 'fl')) %>% rename(fl_price = price) %>% head
 mpg
+
+
+data
+smdt
+prePar = par(col='red')
+plot(smdt)
+par(prePar)
+plot(data$eng, main='영어성적')
+
+theme_set(theme_gray(base_family="AppleGothic"))
+par(family = "AppleGothic")
+
+plot(x=1, y=1)
+plot(x=1:10, y=1:10)
+plot(sin, -pi, pi * 3)
+sin(30)
+plot(smdt$stuno, smdt$Korean)
+plot(smdt$stuno, smdt$Korean, col='lightblue') 
+
+colors() 
+?points
+
+plot(x = smdt$stuno, y = smdt$Korean,
+     col = '#0000FF',
+     cex = 2,
+     las = 1,
+     type = 'p',         # p, l, b, c, o, s
+     xlim = c(-0.5, 5.5),
+     ylim = c(0, 100),
+     pch = 13,                         # > ?points
+     xlab = '학번', ylab = '국어',
+     main = '그래프 타이틀')
+
+xl = c(-0.5, 8.5); yl = c(0, 100)
+plot(x = smdt$stuno, y = smdt$Korean,
+     col='#0000FF', cex=3, pch = 8,
+     xlim = xl, ylim = yl,
+     xlab = '학번', ylab = '국어, 수학',
+     main = '우리반 국어 / 수학 성적')
+par(new = T)
+plot(x = smdt$stuno, y = smdt$Math,
+     col='#ff0000', cex=3, pch = 21,
+     xlim = xl, ylim = yl,
+     xlab = '', ylab = '')
+
+l=legend('center',
+       legend=c('국어', '수학'),
+       pch=c(8, 21), col=c('blue', 'green'), bg='gray')
+
+
+head(data)
+t = data %>% filter(eng > 90) %>% select('cls') %>% table
+t = data %>% filter(eng > 90) %>% select('cls', 'gen') %>% table
+t
+barplot(t,
+        beside = T,
+        horiz = T,
+        border = 'dark blue',
+        density = 50,
+        las = 1, 
+        cex.names = 0.8,
+        xlim = c(0, 13),
+        main='영어성적 상위권 학생',
+        angle = 15 + 10*1:2,
+        xlab = '학급별 성별', ylab = '영어',
+        legend=rownames(t),
+        col=heat.colors(5))
+
+boxplot(data$kor)
+boxplot(kor~cls, data=data, col='lightblue')
+boxplot(kor~gen, data=data, col='lightblue')
+
+hist(data$kor, col="gray", labels=T, breaks=10)
+
+curve(tan, -2 * pi, 3 * pi,
+      xname='국어', xlab = 'TT',
+      n=200, type='p')
+
+
+d = data %>% filter(kor > 90) %>% select('cls')
+d
+table(d)
+pie(table(d))
+pie(table(d), clockwise = T)
+pie(table(d), clockwise = T, col=c('red', 'purple', 'green', 'cyan'))
+
+save(data, file='data/data_eng.rda')
+data
+
+
+## ggplot2 ############
+smdt
+
+ggplot() + geom_line(data=smdt, aes(x=stuno, y=Korean))
+smdt
+ggplot() + geom_point(data=smdt, aes(x=stuno, y=Korean))
+
+ggplot(data=smdt) +
+  geom_point(aes(x=stuno, y=Korean),
+             color='blue', size = 5)
+
+ggplot(data=smdt) +
+  geom_point(aes(x=stuno, y=Korean, color=stuno),
+             size = 5)
+
+ggplot(data=smdt) +
+  geom_point(aes(x=stuno, y=Korean, 
+                 color=stuno, size=stuno))
+
+data
+d = data %>% filter(stuno >= 30000)
+d
+ggplot(d %>% filter(kor > 80), aes(cls, kor)) +
+  geom_point(aes(color=cls, size=kor), 
+             alpha=0.3)
+
+ggplot(mpg, aes(cty, hwy)) + geom_point() + xlim(20,25)
+
+## line ######
+mpg = as.data.frame(ggplot2::mpg)
+head(mpg)
+nrow(mpg)
+d2 = mpg %>%
+  group_by(manufacturer, displ) %>% 
+  summarise(m1 = mean(cty), m2 = mean(hwy))
+d2
+ggplot(d2, aes(x=displ)) + 
+  geom_line(aes(y=m1, color='cty')) + 
+  geom_line(aes(y=m2, color='hwy'), size=1) +
+  scale_colour_manual("", breaks = c("cty", "hwy"),
+                      values = c("red", "blue")) +
+  xlab("x축") +
+  xlim(1, 8) +
+  scale_y_continuous("y축", limits = c(5, 45)) +
+  labs(title = '타이틀', subtitle = '서브 타이틀') 
+
+
+ggplot(mpg, aes(displ)) +
+  geom_histogram(aes(fill=class), 
+                 # binwidth = .3,
+                 bins = 5,
+                 col='black',       # line color
+                 size=.1) +         # line size
+  labs(title = 'Title', subtitle = 'Sub Title', fill='등급')
+
+
+ggplot(mpg, aes(manufacturer)) +
+  geom_bar(aes(fill=class),
+           width = 0.9) +
+  theme(axis.text.x = element_text(angle=85,  
+                                   vjust=0.6)) +  
+  scale_fill_discrete(name = "class") + 
+  labs(title = 'Title', subtitle = 'Sub Title')
+
+
+ggplot(mpg, aes(cty)) +
+  geom_density(aes(fill=factor(cyl)), alpha=0.9) +
+  labs(title="밀도그래프", subtitle = "실린더수에 따른 시내연비의 밀도그래프",
+       caption="Source: ggplot2::mpg",
+       x = "도시 연비",
+       fill = "실린더수")
+
+midwest
+
+install.packages('gridExtra')
+library(gridExtra)
+
+g3 = ggplot(mpg, aes(manufacturer)) +
+  geom_bar(aes(fill=class),
+           width = 0.7) +
+  theme(axis.text.x = element_text(angle=45,
+                                   vjust=0.6)) +
+  labs(title = 'Title', subtitle = 'Sub Title')
+
+g4 = ggplot(mpg, aes(cty)) +
+  geom_density(aes(fill=factor(cyl)), alpha=0.8) +
+  labs(title="밀도그래프", subtitle = "실린더수에 따른 시내연비의 밀도그래프",
+       caption="Source: ggplot2::mpg",
+       x = "도시 연비",
+       fill = "실린더수")
+
+g3
+grid.arrange(g3, grid.arrange(g2, g4, ncol=2), ncol=1)
