3e7b653eacc298501dbb337612a8faf11f7d34c4
indiflex <indiflex1@gmail.com>
2019-03-25 12:48:26 +0900

    2019-03-25 강의

--- twitter.R
+++ twitter.R
@@ -18,10 +18,7 @@ install.packages(c("rJava", "memoise", "KoNLP"))
 library(rJava)
 library(KoNLP)
 
-api_key = "TQA3hvxsZr2Fwfrf03slKKNxB"
-api_secret = "Vzlrg6fgOQm1T8SyNRuARwhi5Z5UJP04KhhwLu4KPJ8f0WwmID"
-token = "1905223106-1pUZ0Eqvz9ojDMvwVwcC4Sm8Lqs8OHkQlz3xiRn"
-token_secret = "bE4hyDyB7e6ZyODJV8RTLpj3AcoqPmYk7QtI4m9XmV0YG"
+# keys are keys.R
 setup_twitter_oauth(api_key, api_secret, token, token_secret)
 
 # searchTwitter(enc2utf8('승리'), n=100, lan='ko')
